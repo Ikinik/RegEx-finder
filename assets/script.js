73dca6eefe2614565ab8e7839bc4fbfbeed2120b
Ikinik <adam.sorfa@gmail.com>
2016-12-02 20:11:11 +0100

    Pridano oznacovani skupin v regularnim vyrazu. + osetreny nebezpecne vstupy do regularu

--- assets/script.js
+++ assets/script.js
@@ -110,8 +110,9 @@ function menu() {
 		var regexInput = $("input#regex");
 		var regexString = regexInput.val();
         var regexChar = $(this).text();
-
-        //special behavior
+		var quantifable = $(this).hasClass("quantifiable");
+		
+        //char changes
         switch(regexChar){
             case "{n}":
                 regexChar = "{1}";
@@ -128,15 +129,23 @@ function menu() {
         }
 
         if(regexString.length >= caretStart){
-            var before = regexString.substring(0,caretStart);
-            var after = regexString.substring(caretStart, regexString.length);
-            regexString = before + regexChar + after;
-            regexInput.val(regexString);
+            if(quantifable && (caretStart != caretEnd)){
+				var before = regexString.substring(0,caretStart);
+				var content = regexString.substring(caretStart, caretEnd);
+				var after = regexString.substring(caretEnd, regexString.length);
+				regexString = before + "(" + content + ")" + regexChar + after;  
+			}else{			
+				var before = regexString.substring(0,caretStart);
+				var after = regexString.substring(caretStart, regexString.length);
+				regexString = before + regexChar + after;
+				caretStart = caretEnd = before.length + regexChar.length;
+			}
+			regexInput.val(regexString);
         }else{
             regexInput.val(regexString + regexChar);
         }
-
 		regexInput.val(regexString);
+		searchMatches();
 	});
 }
 
@@ -164,31 +173,37 @@ function searchMatches(){
 	var matchesColors = ['#ef2765', '#01adef', '#8cc63e', '#662e93', '#f79437', '#00b6ba', '#ef3e36', '#f9ee06',
 	'#c3b59b', '#30b66f', '#48bfe9', '#3cae4c', '#f36056', '#9a65ab', '#fbac1c', '#aac47d', '#8f8f8d', '#6490cd',
 	'#f06aa7', '#c5996c', '#a01f62', '#0f75bd', '#ed1b24', '#92c741'];
-
+	
 	matchesText.find('.match').remove();
 	matchesCount.html("0");
 	var regexString = $("input#regex").val();
-	if (regexString != "") {
-		var regex = new RegExp(regexString, regexFlags);
-		var content = textArea.val();
-		var result = content.match(regex);
-		var matches = [];
-		while((match = regex.exec(content)) != null){
-			matches.push(match);
-		}
-		
-		if(matches.length != 0){
-			matchesCount.html(matches.length);
-			matches.forEach(function(matchedEl){
-				var color = matchesColors[Math.floor((Math.random() * (matchesColors.length - 1)) + 0)];
-				var matchDivString = "<div class='match' style='background-color: " + color + "' data-position='"+matchedEl["index"]+"'>" + matchedEl[0] + "</div> ";
-				var matchDiv = $(matchDivString).click(function(){
-					textArea.focus();
-					setSelectionRange(textArea[0], matchedEl["index"], matchedEl["index"] + matchedEl[0].length);
-					
+	
+	if (checkRegex(regexString)) {
+		try{
+			var regex = new RegExp(regexString, regexFlags);		
+			var content = textArea.val();
+			var result = content.match(regex);
+			var matches = [];
+			
+			while((match = regex.exec(content)) != null){
+				matches.push(match);
+			}
+			
+			if(matches.length != 0){
+				matchesCount.html(matches.length);
+				matches.forEach(function(matchedEl){
+					var color = matchesColors[Math.floor((Math.random() * (matchesColors.length - 1)) + 0)];
+					var matchDivString = "<div class='match' style='background-color: " + color + "' data-position='"+matchedEl["index"]+"'>" + matchedEl[0] + "</div> ";
+					var matchDiv = $(matchDivString).click(function(){
+						textArea.focus();
+						setSelectionRange(textArea[0], matchedEl["index"], matchedEl["index"] + matchedEl[0].length);
+						
+					});
+					matchDiv.insertBefore(matchesText.find("div.ender"));
 				});
-				matchDiv.insertBefore(matchesText.find("div.ender"));
-			});
+			}
+		}catch(e){
+			return;
 		}
 	}	
 }
@@ -205,4 +220,15 @@ function setSelectionRange(input, selectionStart, selectionEnd) {
     range.moveStart('character', selectionStart);
     range.select();
   }
+}
+
+function checkRegex(testedRexEx){
+	var dangerousPatterns = /(\^+)|(\$*)|((\\)b*)|((\\)B*)/;
+	//pattern axplantion (           ^       ) (      \b     ) (      \B     ) 
+	dangerousPatterns = /(^\^+(\\)?$)|(\^{2,})|(^(\\b)+(\\)?$)|(^(\\B)+(\\)?$)/;
+	if(testedRexEx != ""){
+		return !dangerousPatterns.test(testedRexEx);
+	}else{
+		return false;
+	}
 }
